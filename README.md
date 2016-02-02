b626fe1ccbf7e4d54e202a107fed2cf74a4fdee2
Michael Brodhead <mkb@orthogonal.org>
2016-02-02 10:40:59 -0800

    simple counter

---
+++ README.md
@@ -0,0 +1,9 @@
+# Trivial web counter
+
+This example app is intended for demonstrating and teaching
+[BOSH](https://bosh.io) deployments.
+
+The app displays a page with a counter. The counter is incremented
+each time the page is reloaded.
+
+We require Redis for storing the counter.
--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
+require 'redis/objects'
+require 'haml'
+
+redis_host = ENV['REDIS_HOST'] || '127.0.0.1'
+Redis.current = Redis.new(:host => redis_host, :port => 6379)
 
 get '/' do
-  "Hello world"
+  counter = Redis::Counter.new('page_views')
+  @views = counter.increment
+  haml :index
 end
 
+get '/reset' do
+  counter = Redis::Counter.new('page_views')
+  counter.reset
+  redirect '/'
+end
