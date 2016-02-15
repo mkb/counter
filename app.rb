445e9c10c21b11f1404ba079edebc249ee48131b
Michael Brodhead <mkb@orthogonal.org>
2016-02-15 14:28:42 -0800

    Nice error if we can't connect to Redis.

--- app.rb
+++ app.rb
@@ -6,9 +6,15 @@ redis_host = ENV['REDIS_HOST'] || '127.0.0.1'
 Redis.current = Redis.new(:host => redis_host, :port => 6379)
 
 get '/' do
-  counter = Redis::Counter.new('page_views')
-  @views = counter.increment
-  haml :index
+  begin
+    counter = Redis::Counter.new('page_views')
+    @views = counter.increment
+    haml :index
+  rescue Redis::CannotConnectError
+    'Could not connect to Redis. Make sure Redis is running and set ' +
+    'REDIS_HOST if Redis is not on localhost.'
+  end
+
 end
 
 get '/reset' do
